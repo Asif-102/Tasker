f8040b449421b64cfeba1347aa152ffdbde54454
Asif-102 <asif1zero2@gmail.com>
2024-10-14 03:20:31 +0600

    Hero Section created done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Footer from "./Footer";
 import Header from "./Header";
+import HeroSection from "./HeroSection";
 
 export default function App() {
   return (
     <>
       <Header />
+      <HeroSection />
       <Footer />
     </>
   );
