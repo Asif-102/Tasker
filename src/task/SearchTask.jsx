945c2a1fd8190ce64a20b5fbac189209f85c5944
Asif-102 <asif1zero2@gmail.com>
2024-10-15 04:15:27 +0600

    search implemented done

--- src/task/SearchTask.jsx
+++ src/task/SearchTask.jsx
@@ -1,4 +1,14 @@
-export default function SearchTask() {
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+
+export default function SearchTask({ onSearch }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function handleClick(evt) {
+    evt.preventDefault();
+    onSearch(searchTerm);
+  }
+
   return (
     <form>
       <div className="flex">
@@ -8,11 +18,14 @@ export default function SearchTask() {
             id="search-dropdown"
             className="z-20 block w-full bg-gray-800 px-4 py-2 pr-10 focus:outline-none"
             placeholder="Search Task"
+            value={searchTerm}
+            onChange={(evt) => setSearchTerm(evt.target.value)}
             required
           />
           <button
             type="submit"
             className="absolute right-2 top-0 h-full rounded-e-lg text-white md:right-4"
+            onClick={handleClick}
           >
             <svg
               className="h-4 w-4"
--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -63,6 +63,16 @@ export default function TaskBoard() {
     setTaskToUpdate(null);
   }
 
+  function handleSearch(searchTerm) {
+    console.log(searchTerm);
+
+    const filtered = tasks.filter((task) =>
+      task.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setTasks([...filtered]);
+  }
+
   return (
     <section className="mb-20" id="tasks">
       {showAddModal && (
@@ -74,7 +84,7 @@ export default function TaskBoard() {
       )}
       <div className="container">
         <div className="p-2 flex justify-end">
-          <SearchTask />
+          <SearchTask onSearch={handleSearch} />
         </div>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskActions
