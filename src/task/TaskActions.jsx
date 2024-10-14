994f22501ec01e1d842196e70593193428421e4f
Asif-102 <asif1zero2@gmail.com>
2024-10-15 03:50:33 +0600

    delete all tasks feature enable

--- src/task/TaskActions.jsx
+++ src/task/TaskActions.jsx
@@ -1,4 +1,5 @@
-export default function TaskActions({ onAddClick }) {
+/* eslint-disable react/prop-types */
+export default function TaskActions({ onAddClick, onDeleteAllClick }) {
   return (
     <div className="mb-14 items-center justify-between sm:flex">
       <h2 className="text-2xl font-semibold max-sm:mb-4">Your Tasks</h2>
@@ -9,7 +10,10 @@ export default function TaskActions({ onAddClick }) {
         >
           Add Task
         </button>
-        <button className="rounded-md bg-red-500 px-3.5 py-2.5 text-sm font-semibold">
+        <button
+          className="rounded-md bg-red-500 px-3.5 py-2.5 text-sm font-semibold"
+          onClick={onDeleteAllClick}
+        >
           Delete All
         </button>
       </div>
--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -44,6 +44,11 @@ export default function TaskBoard() {
     setTasks(tasksAfterDelete);
   }
 
+  function handleDeleteAllClick() {
+    tasks.length = 0;
+    setTasks([...tasks]);
+  }
+
   function handleCloseClick() {
     setShowAddModal(false);
     setTaskToUpdate(null);
@@ -63,7 +68,10 @@ export default function TaskBoard() {
           <SearchTask />
         </div>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskActions onAddClick={() => setShowAddModal(true)} />
+          <TaskActions
+            onAddClick={() => setShowAddModal(true)}
+            onDeleteAllClick={handleDeleteAllClick}
+          />
           <TaskList
             tasks={tasks}
             onEdit={handleEditTask}
