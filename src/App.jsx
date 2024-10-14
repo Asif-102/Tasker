cdf32ef6b6036ef136a3dbb832f7db41e2630328
Asif-102 <asif1zero2@gmail.com>
2024-10-14 14:35:25 +0600

    Task Board manage done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import HeroSection from "./HeroSection";
+import TaskBoard from "./task/TaskBoard";
 
 export default function App() {
   return (
     <>
       <Header />
-      <HeroSection />
+      <div className="flex flex-col justify-center items-center">
+        <HeroSection />
+        <TaskBoard />
+      </div>
       <Footer />
     </>
   );
