8d3510615be293fafc232d1aabc8a6efbac63104
Asif-102 <asif1zero2@gmail.com>
2024-10-15 01:25:51 +0600

    load task lists dynamically

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
 
 export default function TaskBoard() {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learn React",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus numquam ex similique exercitationem fuga, culpa quod esse. Qui, possimus cupiditate?",
+    tags: ["web", "react", "js"],
+    priority: "High",
+    isFavorite: true,
+  };
+  const [tasks, setTasks] = useState([defaultTask]);
+
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
@@ -11,7 +23,7 @@ export default function TaskBoard() {
         </div>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskActions />
-          <TaskList />
+          <TaskList tasks={tasks} />
         </div>
       </div>
     </section>
